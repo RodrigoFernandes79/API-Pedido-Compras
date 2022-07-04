f40aadfa75a94b10e35f73b02559a3133267d1ea
Rodrigo Fernandes <83513696+RodrigoFernandes79@users.noreply.github.com>
2022-07-04 20:44:00 -0300

    Criando operação de instanciação

--- src/main/java/com/rodrigohf/apicompras/ApicomprasApplication.java
+++ src/main/java/com/rodrigohf/apicompras/ApicomprasApplication.java
@@ -1,13 +1,38 @@
 package com.rodrigohf.apicompras;
 
+import java.sql.Array;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rodrigohf.apicompras.domain.Categoria;
+import com.rodrigohf.apicompras.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ApicomprasApplication {
+public class ApicomprasApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository catRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApicomprasApplication.class, args);
+	
+		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		catRepo.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
--- src/main/java/com/rodrigohf/apicompras/controllers/CategoriaController.java
+++ src/main/java/com/rodrigohf/apicompras/controllers/CategoriaController.java
@@ -1,6 +1,6 @@
 package com.rodrigohf.apicompras.controllers;
 
-import java.util.Optional;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
