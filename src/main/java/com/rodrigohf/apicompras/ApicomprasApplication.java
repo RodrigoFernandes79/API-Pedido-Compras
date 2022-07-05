a9a36ddec08a132cde5b6ceac3b89304b181b8c6
Rodrigo Fernandes <83513696+RodrigoFernandes79@users.noreply.github.com>
2022-07-04 21:39:17 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/rodrigohf/apicompras/ApicomprasApplication.java
+++ src/main/java/com/rodrigohf/apicompras/ApicomprasApplication.java
@@ -1,6 +1,6 @@
 package com.rodrigohf.apicompras;
 
-import java.sql.Array;
+
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,13 +9,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.rodrigohf.apicompras.domain.Categoria;
+import com.rodrigohf.apicompras.domain.Produto;
 import com.rodrigohf.apicompras.repositories.CategoriaRepository;
+import com.rodrigohf.apicompras.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class ApicomprasApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository catRepo;
+	@Autowired
+	private ProdutoRepository prodRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApicomprasApplication.class, args);
@@ -31,7 +35,21 @@ public class ApicomprasApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		catRepo.saveAll(Arrays.asList(cat1,cat2));
+		prodRepo.saveAll(Arrays.asList(p1,p2,p3));
 		
 	}
 
