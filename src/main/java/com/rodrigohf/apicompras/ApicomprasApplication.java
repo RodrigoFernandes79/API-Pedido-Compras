c8c4b1b925f99986374a79f93883bc0850c37a58
Rodrigo Fernandes <83513696+RodrigoFernandes79@users.noreply.github.com>
2022-07-20 22:12:55 -0300

    Armazenamento de imagens usando Amazon S3:Salvando primeiro arquivo no S3

--- src/main/java/com/rodrigohf/apicompras/ApicomprasApplication.java
+++ src/main/java/com/rodrigohf/apicompras/ApicomprasApplication.java
@@ -1,20 +1,27 @@
 package com.rodrigohf.apicompras;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rodrigohf.apicompras.services.S3Service;
+
 @SpringBootApplication
 public class ApicomprasApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApicomprasApplication.class, args);
-
+		
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		
+		s3.uploadFile("C:\\Users\\rodri\\OneDrive\\Documentos\\work.png");
 	}
 
 }
