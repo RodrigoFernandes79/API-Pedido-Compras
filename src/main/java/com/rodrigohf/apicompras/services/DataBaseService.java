e583f36da70a523073fa5f4b67c51af90567190f
Rodrigo Fernandes <83513696+RodrigoFernandes79@users.noreply.github.com>
2022-07-24 12:08:14 -0300

    Ajustes Finais:Acrescentando mais produtos para testar infinity scroll

--- src/main/java/com/rodrigohf/apicompras/services/DataBaseService.java
+++ src/main/java/com/rodrigohf/apicompras/services/DataBaseService.java
@@ -78,7 +78,8 @@ public class DataBaseService {
 		Produto p9 = new Produto(null, "Perfume Pacco Rabbane", 200.00);
 		Produto p10 = new Produto(null, "Corsa Sedan 2011", 22300.00);
 		Produto p11 = new Produto(null, "Apartamento Decorado 3 quartos", 320000.00);
-
+		
+		
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2, p4));
 		cat3.getProdutos().addAll(Arrays.asList(p5, p6));
@@ -87,6 +88,91 @@ public class DataBaseService {
 		cat6.getProdutos().addAll(Arrays.asList(p8));
 		cat7.getProdutos().addAll(Arrays.asList(p10));
 		cat8.getProdutos().addAll(Arrays.asList(p11));
+		
+		Produto p12 = new Produto(null, "Pen Drive", 18.00);
+		Produto p13 = new Produto(null, "Cabo Usb", 22.00);
+		Produto p14 = new Produto(null, "Notebook", 3.000);
+		Produto p15 = new Produto(null, "Fone de ouvido", 80.00);
+		Produto p16 = new Produto(null, "Carregador para Notebook", 120.00);
+		Produto p17 = new Produto(null, "Processador Intel", 830.00);
+		Produto p18 = new Produto(null, "Capa para Computador", 223.00);
+		Produto p19 = new Produto(null, "Computador Celeron 5giga", 4325.00);
+		Produto p20 = new Produto(null, "HD externo SSD", 352.00);
+		Produto p21 = new Produto(null, "Cabo HDMI", 56.00);
+		Produto p22 = new Produto(null, "Notebook Lenovo", 3500.00);
+		Produto p23 = new Produto(null, "Notebook Samsung", 4020.00);
+		Produto p24 = new Produto(null, "Ipad 60", 5200.00);
+		Produto p25 = new Produto(null, "Carregador para Ipad", 210.00);
+		Produto p26 = new Produto(null, "Tablet Samsung", 3800.00);
+		Produto p27 = new Produto(null, "Teclados", 180.00);
+		Produto p28 = new Produto(null, "Caixa de Som", 120.00);
+		Produto p29 = new Produto(null, "Mouse pads", 21.00);
+		Produto p30 = new Produto(null, "Cartão de Memória", 223.00);
+		Produto p31 = new Produto(null, "PC Intel", 1.250);
+		Produto p32 = new Produto(null, "Computador Pentium 8° Geração", 2198.00);
+		Produto p33 = new Produto(null, "Mini Conversor de Vídeo", 59.00);
+		Produto p34 = new Produto(null, "Conector Transparente", 38.00);
+		Produto p35 = new Produto(null, "Emenda Cabo de Rede", 6.00);
+		Produto p36 = new Produto(null, "Roteador Intelbras", 338.00);
+		Produto p37 = new Produto(null, "Connectv Digital", 138.00);
+		Produto p38 = new Produto(null, "Receptor de TV Digital", 155.00);
+		Produto p39 = new Produto(null, "Placa de Vídeo",145.00);
+		Produto p40 = new Produto(null, "Kit Upgrade 10° Geração", 2112.00);
+		Produto p41 = new Produto(null, "Kit Upgrade 8° Geração", 1698.00);
+		Produto p42 = new Produto(null, "Placa Mãe Kronus", 648.00);
+		Produto p43 = new Produto(null, "Placa Mãe Ipx",624.00);
+		Produto p44 = new Produto(null, "Cpu Gamer Rizer", 2332.00);
+		Produto p45 = new Produto(null, "Webcan Multilaser", 312.00);
+		Produto p46 = new Produto(null, "Conversor Usb", 43.00);
+		Produto p47 = new Produto(null, "Extensão Portas Usb", 32.00);
+		Produto p48 = new Produto(null, "Usb Virtual Channel Adapter", 18.00);
+		Produto p49 = new Produto(null, "Emenda HDMI", 16.00);
+		Produto p50 = new Produto(null, "Conversor HDMI", 28.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p12, p13, p14, p15, p16, p17, p18, p19, p20,
+		p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p34, p35, p36, p37, p38,
+		p39, p40, p41, p42, p43, p44, p45, p46, p47, p48, p49, p50));
+		
+		p12.getCategorias().addAll(Arrays.asList(cat1));
+		p13.getCategorias().addAll(Arrays.asList(cat1));
+		p14.getCategorias().addAll(Arrays.asList(cat1));
+		p15.getCategorias().addAll(Arrays.asList(cat1));
+		p16.getCategorias().addAll(Arrays.asList(cat1));
+		p17.getCategorias().addAll(Arrays.asList(cat1));
+		p18.getCategorias().addAll(Arrays.asList(cat1));
+		p19.getCategorias().addAll(Arrays.asList(cat1));
+		p20.getCategorias().addAll(Arrays.asList(cat1));
+		p21.getCategorias().addAll(Arrays.asList(cat1));
+		p22.getCategorias().addAll(Arrays.asList(cat1));
+		p23.getCategorias().addAll(Arrays.asList(cat1));
+		p24.getCategorias().addAll(Arrays.asList(cat1));
+		p25.getCategorias().addAll(Arrays.asList(cat1));
+		p26.getCategorias().addAll(Arrays.asList(cat1));
+		p27.getCategorias().addAll(Arrays.asList(cat1));
+		p28.getCategorias().addAll(Arrays.asList(cat1));
+		p29.getCategorias().addAll(Arrays.asList(cat1));
+		p30.getCategorias().addAll(Arrays.asList(cat1));
+		p31.getCategorias().addAll(Arrays.asList(cat1));
+		p32.getCategorias().addAll(Arrays.asList(cat1));
+		p33.getCategorias().addAll(Arrays.asList(cat1));
+		p34.getCategorias().addAll(Arrays.asList(cat1));
+		p35.getCategorias().addAll(Arrays.asList(cat1));
+		p36.getCategorias().addAll(Arrays.asList(cat1));
+		p37.getCategorias().addAll(Arrays.asList(cat1));
+		p38.getCategorias().addAll(Arrays.asList(cat1));
+		p39.getCategorias().addAll(Arrays.asList(cat1));
+		p40.getCategorias().addAll(Arrays.asList(cat1));
+		p41.getCategorias().addAll(Arrays.asList(cat1));
+		p42.getCategorias().addAll(Arrays.asList(cat1));
+		p43.getCategorias().addAll(Arrays.asList(cat1));
+		p44.getCategorias().addAll(Arrays.asList(cat1));
+		p45.getCategorias().addAll(Arrays.asList(cat1));
+		p46.getCategorias().addAll(Arrays.asList(cat1));
+		p47.getCategorias().addAll(Arrays.asList(cat1));
+		p48.getCategorias().addAll(Arrays.asList(cat1));
+		p49.getCategorias().addAll(Arrays.asList(cat1));
+		p50.getCategorias().addAll(Arrays.asList(cat1));
+	
 
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
@@ -102,7 +188,11 @@ public class DataBaseService {
 
 		catRepo.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8));
 		prodRepo.saveAll(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11));
-
+		
+		prodRepo.saveAll(Arrays.asList(p12, p13, p14, p15, p16, p17, p18, p19, p20,
+				p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p34, p35, p36, p37, p38,
+				p39, p40, p41, p42, p43, p44, p45, p46, p47, p48, p49, p50));
+		
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
 
